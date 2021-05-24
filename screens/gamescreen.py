bc76f22cb0fb9e2cf9679be9b77551122ef593ef
CeNiEi <ryujinjakka216@gmail.com>
2021-05-24 21:09:29 +0530

    I/O fixes

--- screens/gamescreen.py
+++ screens/gamescreen.py
@@ -1,10 +1,18 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
-import random
+from random import choice
+
+import widgets.keyboard as keyboard
+
 Builder.load_file('screens/gamescreen.kv')
 
 class GameScreen(Screen):
+    word = str()
 
-    pass  
+    def find_word(self, afile='Dict.txt'):
+        return choice(list(open(afile))).rstrip()
 
-
+    def on_enter(self):
+        GameScreen.word = self.find_word()
+        keyboard.KeyBoard.correct_guess = 0
+        keyboard.KeyBoard.wrong_guess = 0
--- widgets/keyboard.py
+++ widgets/keyboard.py
@@ -1,47 +1,25 @@
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.lang.builder import Builder
-import random
+
+import screens.gamescreen as gamescreen
+
 Builder.load_file('widgets/keyboard.kv')
 
 class KeyBoard(RelativeLayout):
-    wrong_guess = 0
     correct_guess = 0
+    wrong_guess = 0
 
-    #to select a random string
-    def FindHangmanWord(self):
-        afile = 'Dict.txt'
-
-        # find number of lines in file
-        with open(afile) as f:
-            for i, l in enumerate(f):
-                pass
-
-        # grab a random line and return it
-        randNum = random.randrange(i+1)
-        with open(afile) as f:
-            for i, line in enumerate(f):
-                if i+1 >= randNum:
-                    return line[:len(line)-1]
-                    break
-        return None
-
-    #to check if word present in string
     def check_in_word(self, btn):
-        count = 0;
-        for i, letter in enumerate(self.FindHangmanWord()):
-            if btn.text == letter.upper():
-                self.correct_guess+=1
-                count += 1
-                
-                if self.correct_guess >= len(self.FindHangmanWord()):
-                    print('GAME OVER, YOU WIN')
-                
-        if count == 0:
-            # count wrong guess
-            self.wrong_guess+=1
-            if self.wrong_guess >= 6:
-                print('GAME OVER, YOU LOSE!')
+        word = gamescreen.GameScreen.word
 
-        btn.disabled = True # True is synonymous with         
-     
-   
+        if btn.text.lower() in word:
+            KeyBoard.correct_guess += 1
+        else:  
+            KeyBoard.wrong_guess += 1;
+
+        if KeyBoard.correct_guess >= len(set(word)):
+            print("YOU WIN")
+        elif KeyBoard.wrong_guess >= 6:
+            print("YOU LOSE")
+
+        btn.disabled = True
