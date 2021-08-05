80838d1cf3cfa1a931275dac34653a4d244401d8
CeNiEi <ryujinjakka216@gmail.com>
2021-08-05 07:04:09 +0530

    fixed the popups

--- screens/gamescreen.py
+++ screens/gamescreen.py
@@ -13,27 +13,11 @@ Builder.load_file('screens/gamescreen.kv')
 class GameScreen(Screen):
     word = str()
     drawing_area = ObjectProperty(None)
-    keyboard_reference = None
 
-    def find_word(self, afile='Dict.txt'):
-        return choice(list(open(afile))).rstrip()
-
-    def on_enter(self):
-        GameScreen.word = self.find_word()
-        widgets.keyboard.KeyBoard.correct_guess = 0
-        widgets.keyboard.KeyBoard.wrong_guess = 0
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        choose_word()
         widgets.keyboard.KeyBoard.game_screen_reference = self
 
-        for instr in widgets.dynamicwidget.DynamicLineWidget.on_canvas:
-            self.ids.drawing_area.canvas.remove(instr)
-
-        widgets.dynamicwidget.DynamicLineWidget.on_canvas.clear()
-        widgets.dynamicwidget.DynamicLineWidget.instructions.clear()
-
-        GameScreen.keyboard_reference.clear_buttons()
-
-
-
-
-
-
+def choose_word(afile='Dict.txt'):
+    GameScreen.word = choice(list(open(afile))).rstrip()
--- widgets/keyboard.py
+++ widgets/keyboard.py
@@ -1,6 +1,10 @@
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.lang.builder import Builder
 from kivy.graphics import Line, Ellipse
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivymd.uix.button import MDFillRoundFlatIconButton
+from kivy.uix.gridlayout import GridLayout
 
 import screens.gamescreen
 import widgets.dynamicwidget
@@ -12,12 +16,10 @@ class KeyBoard(RelativeLayout):
     wrong_guess = 0
     game_screen_reference = None
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        screens.gamescreen.GameScreen.keyboard_reference = self
-
     def check_in_word(self, btn):
         word = screens.gamescreen.GameScreen.word
+        print(word)
+        btn.disabled = True
 
         if btn.text.lower() in word:
             KeyBoard.correct_guess += 1
@@ -29,8 +31,7 @@ class KeyBoard(RelativeLayout):
             print("YOU WIN")
         elif KeyBoard.wrong_guess >= 6:
             print("YOU LOSE")
-
-        btn.disabled = True
+            self.lose_popup(word)
 
     def clear_buttons(self):
         for btn in self.ids.values():
@@ -68,3 +69,34 @@ class KeyBoard(RelativeLayout):
                 widgets.dynamicwidget.DynamicLineWidget(
                         bezier=[610, 480, 630, 500, 680, 390],
                         width=10, parent=KeyBoard.game_screen_reference.ids.drawing_area)
+
+    def lose_popup(self, hidden_word):
+        layout = GridLayout(cols=1, padding=10)
+
+        label = Label(text="The hidden word was "+hidden_word)
+        closebutton = MDFillRoundFlatIconButton(text="Retry",
+                icon="rotate-right", pos_hint={'center_x':.7, 'center_y':.5})
+        
+        layout.add_widget(label)
+        layout.add_widget(closebutton)
+
+        self.reset()
+
+        popup = Popup(title="YOU LOSE!", content= layout, size_hint=(None, None), 
+                size=(300, 300), auto_dismiss=False)
+
+        popup.open()
+        closebutton.bind(on_press=popup.dismiss)
+
+    def reset(self):
+        for instr in widgets.dynamicwidget.DynamicLineWidget.on_canvas:
+            KeyBoard.game_screen_reference.ids.drawing_area.canvas.remove(instr)
+
+        widgets.dynamicwidget.DynamicLineWidget.on_canvas.clear()
+        widgets.dynamicwidget.DynamicLineWidget.instructions.clear()
+
+        KeyBoard.wrong_guess = 0
+        KeyBoard.correct_guess = 0
+
+        self.clear_buttons()
+        screens.gamescreen.choose_word()
