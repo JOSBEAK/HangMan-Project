6cab4b773fc46f949f2793e7d5bda99116321334
Anurag Pandey <josbeak@Josbeak.localdomain>
2021-08-25 09:33:01 +0530

    Modified Title Screen

---
+++ backgrounds/background3.py
@@ -0,0 +1,7 @@
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+Builder.load_file('backgrounds/background3.kv')
+
+class BackGround3(Widget):
+    pass
--- screens/titlescreen.py
+++ screens/titlescreen.py
@@ -14,10 +14,14 @@ class TitleScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        anim = Animation(sz_offset=.7, duration=3)
+        anim  = Animation(opacity=0, duration=0.1) + Animation(opacity=0, duration=1)
+        anim += Animation(opacity=1, duration=0.1) + Animation(opacity=1, duration=1)
+        anim.repeat = True
         anim.start(self)
-
-        Clock.schedule_once(lambda *args: self.change_screen(), 5)
+            
+    def on_touch_down(self,touch):
+        if touch.is_double_tap:
+            self.change_screen()
 
     def change_screen(self):
         app_root = MDApp.get_running_app().root
