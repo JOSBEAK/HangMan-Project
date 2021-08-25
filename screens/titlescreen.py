2d703f7add48fd999786c1f686a71605a79ee4c2
CeNiEi <ryujinjakka216@gmail.com>
2021-08-25 10:46:12 +0530

    fix title screen

--- screens/titlescreen.py
+++ screens/titlescreen.py
@@ -1,21 +1,21 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
-from kivy.properties import NumericProperty, StringProperty
+from kivy.properties import NumericProperty
 from kivy.animation import Animation
-from kivy.clock import Clock
 
 from kivymd.app import MDApp
 
 Builder.load_file('screens/titlescreen.kv')
 
 class TitleScreen(Screen):
-    sz_offset = NumericProperty(0)
+    col_offset = NumericProperty(0)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        anim  = Animation(opacity=0, duration=0.1) + Animation(opacity=0, duration=1)
-        anim += Animation(opacity=1, duration=0.1) + Animation(opacity=1, duration=1)
+        anim = Animation(col_offset=1)
+        anim += Animation(col_offset=0)
+
         anim.repeat = True
         anim.start(self)
             
@@ -25,7 +25,6 @@ class TitleScreen(Screen):
 
     def change_screen(self):
         app_root = MDApp.get_running_app().root
-
         setattr(app_root, 'current', '_main_screen_')
 
     
